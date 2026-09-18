ec469020c4f69e6e28873f27a4312fbb7cf41ac8
Imraj <imrajrabbani2003@gmail.com>
2026-09-18 23:31:29 +0600

    Write backend API endpoints

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import "dotenv/config"
+import express from "express"
+import { signUp, signIn, profile } from "./controllers/auth"
+import { verifyToken } from "./middleware/auth"
+
+const app = express()
+app.use(express.json())
+
+app.post("/signup", signUp)
+app.post("/signin", signIn)
+app.get("/profile", verifyToken, profile)
+
+app.listen(3000)
